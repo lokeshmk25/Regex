e5074d0d4814adf59df3c20278f99ad1a2d5e443
lokeshmk25 <lokeshmk2211@gmail.com>
2021-08-23 14:12:25 +0530

    Added:First Name pattern

--- src/com/Regex/Regexconcept.java
+++ src/com/Regex/Regexconcept.java
@@ -1,7 +1,18 @@
 package com.Regex;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Regexconcept {
     public static void main(String[] args) {
-        System.out.println("Welcome to regex problems");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Name starts with cap and has minimum 3 charcters");
+        System.out.println("Enter the firstname :");
+        String  s=scanner.nextLine();
+        Pattern pattern = Pattern.compile("[A-Z][a-z]{2,}");
+        Matcher matcher = pattern.matcher(s);
+        boolean ismatched=matcher.matches();
+
+        System.out.println(ismatched);
     }
 }
